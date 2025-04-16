de693a0f7c7b25753acd73347240d62f5f0c7be4
HamedMiri72 <hamedmiri72@gmail.com>
2025-04-16 12:32:03 +0100

    entry point for the backend to stat wit express

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+
+
+//app config
+
+const app = express();
+const port = process.env.PORT || 4000
+
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+
+// api endpoint
+
+app.get("/", (req, res) => {
+    res.send("APPI WORKING");
+})
+
+app.listen(port, ()=>console.log("Server started", port));
+
+
