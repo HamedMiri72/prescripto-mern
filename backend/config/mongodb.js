c47a427305b080379bddb8b2f318894911765aae
HamedMiri72 <hamedmiri72@gmail.com>
2025-04-18 23:35:16 +0100

    mongo connection

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 
+
 const connectDB = async() => {
 
     try{
 
         const conn = await mongoose.connect(process.env.MONGODB_URI);
-        const host = conn.connection.host;
-        console.log("Mongodb is connected on host:: ", host);
+        console.log("Mongodb is connected on host:: ", conn.connection.host);
+
     }catch(error){
-        console.error("Error in connecting to the database", error.message);
+        console.error("Error in connecting mongoDB: ", error.message);
         process.exit(1);
     }
 }
 
-export default connectDB;
+export default connectDB
